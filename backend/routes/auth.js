9ed6bc1d07b4d999b51b1d6ab11da60977e5773a
Shivam <shivammudotia@IN-FVFF8FJZQ05D.local>
2022-04-18 11:48:54 +0530

    backend completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,42 +1,127 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const { body, validationResult } = require('express-validator');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
 
+//const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 
-// create a user using POST - "/api/post/createuser" , no auth needed. 
-router.post('/createuser', [
-    body('email','Enter a Valid email').isEmail(),
-    body('name', 'Enter a valid Name').isLength({ min: 5 }),
-    body('password','Enter a Valid Password').isLength({ min: 5 })
-    ], async (req, res) => {
-        // If there are validation errors, return bad request and the error(s)
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+//const JWT_SECRET = .process.env.JWT_TOKEN
+//console.log(JWT_SECRET)
 
-        // check if a user with supplied email already exists
-        try {
- 
-        let user =  await User.findOne({email: req.body.email});
-        if(user){
-            return res.status(400).json({ errors: 'A user ith this email is  already registered'});
-        }
-            
-        user = await User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email
-          })
+JWT_SECRET = "Thisisa$ecretJWTToken";
 
-        res.json(user)
-                   
+// ROUTE 1 - create a user using POST - "/api/post/createuser" , no auth needed.
+
+router.post(
+  "/createuser",
+  [
+    body("email", "Enter a Valid email").isEmail(),
+    body("name", "Enter a valid Name").isLength({ min: 5 }),
+    body("password", "Enter a Valid Password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    // If there are validation errors, return bad request and the error(s)
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    catch(error) {
-        console.error(error.message);
-        res.status(500).send("Some Error Occured")
-    }
-})
 
-module.exports = router
+    // check if a user with supplied email already exists
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: "A user ith this email is  already registered" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+
+      data = { 
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// ROUTE 2  - Authenticate a user using POST - "/api/post/login"
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a Valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // If there are validation errors, return bad request and the error(s)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({
+          Error:
+            "Incorrect Credentials. Please Recheck and try with correct credentials ! ",
+        });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({
+          Error:
+            "Incorrect Credentials. Please Recheck and try with correct credentials ! ",
+        });
+      }
+
+      data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// ROUTE 3 - Get Logged in user details using POST - "/api/post/getuser" , Logged in needed.
+
+router.get("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,126 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => {
+// ROUTE 1 - Fetch All notes of a user using GET - "/api/notes/fetchallnotes". Auth Token needed (Login Needed)
 
-    res.json([])
-})
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-module.exports = router
+// ROUTE 2 - Add a new note for a user using POST - "/api/notes/addnote". Auth Token needed (Login Needed)
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a Valid Title with atleast 3 characters ").isLength({
+      min: 3,
+    }),
+    body(
+      "description",
+      "Enter a valid Description with at least 5 characters"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      // If there are validation errors, return bad request and the error(s)
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { title, description, tag } = req.body;
+
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const savedNote = await note.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// ROUTE 3 - Update existing note for a user by note id using PUT - "/api/notes/updatenote/<id>". Auth Token needed (Login Needed)
+
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+
+  try {
+    //create a new note object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    // find a note that needs to be updated and update it.
+
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Note not found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("You are not authorized to update this note");
+    }
+
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json(note);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// ROUTE 4 - Delete existing note for a user by note id using DELETE - "/api/notes/deletenote/<id>". Auth Token needed (Login Needed)
+
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    // find a note that needs to be deleted and delete it.
+
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Note not found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("You are not authorized to delete this note");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+
+    res.json({ Success: "Note deleted !" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
